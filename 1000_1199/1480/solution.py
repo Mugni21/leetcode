fad5a216340a0fc2ab921074c05895c12ea0246c
Andres Mugnier <andresmugnierz@gmail.com>
2026-04-05 19:59:37 -0400

    Solved Running of 1d Array

---
+++ 1000_1199/1480/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        cumsum=0
+        out=[]
+        for i in range(len(nums)):
+            out.append(nums[i]+cumsum)
+            cumsum+=nums[i]
+        return out
