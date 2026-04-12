a3d4c8ca3d269a8d8bb94f29d11e3fe37a3725c7
Andres Mugnier <andresmugnierz@gmail.com>
2026-04-12 17:51:18 -0400

    Solved easy matrix one

---
+++ leetcodesols/1200_1399/1249_766_ToeplitzMatrix/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        m=len(matrix)
+        n=len(matrix[0])
+
+        for i in range(m):
+            for j in range(n):
+                if (i-1)>=0 and (j-1)>=0:
+                    if matrix[i-1][j-1]!=matrix[i][j]:
+                        return False
+        return True
+
+
+
+        
