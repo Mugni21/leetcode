e0a31744917ecf8c4c0cca9db48daf40e2cda2a9
Andres Mugnier <andresmugnierz@gmail.com>
2026-04-14 20:22:04 -0400

    solved Lexicographic Order of Strings one

---
+++ leetcodesols/1400_1599/1405_2734_LexicographicallySmallestStringAfterSubstringOperation⭐/solution.py
@@ -0,0 +1,37 @@
+class Solution:
+    #Super simple solution. Just skip the a's that are like a prefix, and change everyuthing until you see the
+    #next a again! 
+    #If the tdring was just a's, then onluy change the last 'digit'. 
+
+    def smallestString(self, s: str) -> str:
+        n=len(s)
+        i=0
+        while i<n and s[i]=='a':
+            i+=1
+        
+        if i==n:
+            s=s[:-1]+'z'
+            return s
+        news=s[:i]
+        
+        while i<n and s[i]!='a':
+            news+=chr(ord(s[i])-1)
+            i+=1
+        news+=s[i:]
+
+        return news
+
+        
+
+
+
+
+
+        
+
+        
+
+    
+
+                 
+        
