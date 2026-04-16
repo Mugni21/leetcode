b0a1674cb41fe28c31843b7b645d4acba22ba0b0
Andres Mugnier <andresmugnierz@gmail.com>
2026-04-16 12:24:58 -0400

    updated sol

--- leetcodesols/1200_1399/1307_1893_CheckIfAllTheIntegersInARangeAreCovered/solution.py
+++ leetcodesols/1200_1399/1307_1893_CheckIfAllTheIntegersInARangeAreCovered/solution.py
@@ -2,9 +2,8 @@ class Solution:
     def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
         interval=set(range(left,right+1))
         for l,r in ranges:
-            for num in range(left,right+1):
-                if l<=num<=r:
-                    interval.discard(num)
+            for num in range(l,r+1):
+                interval.discard(num)
                 if not interval:
                     return True
         return False
