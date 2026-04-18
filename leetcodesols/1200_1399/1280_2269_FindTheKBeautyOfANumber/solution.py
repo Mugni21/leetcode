4121e56cb0c17285cb91d2f80f3f12259f22ce75
Andres Mugnier <andresmugnierz@gmail.com>
2026-04-18 00:26:57 -0400

    solved string slicing problem

---
+++ leetcodesols/1200_1399/1280_2269_FindTheKBeautyOfANumber/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def divisorSubstrings(self, num: int, k: int) -> int:
+        s=str(num)
+        n=len(s)
+        
+        counter=0
+        for j in range(0,n-k+1):
+            val=s[j:j+k]
+            print(val)
+            if int(val)!=0 and num%int(val)==0:
+                counter+=1
+        
+        return counter
+
+
+        
