a0047d3453a86c5340d9e39dad8a788816512c85
Andres Mugnier <andresmugnierz@gmail.com>
2026-05-27 21:52:38 -0400

    Solved another heap one

---
+++ leetcodesols/1200_1399/1310_1710_MaximumUnitsOnATruck/solution.py
@@ -0,0 +1,25 @@
+import heapq
+#Not optimal, but I'm having fun with heaps
+class Solution:
+
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxes=[(-uni,numbox) for numbox,uni in boxTypes]
+        heapq.heapify(boxes)
+        added=0
+        while truckSize>0 and boxes:
+            uni,numbox=heapq.heappop(boxes)
+            if numbox>0:
+                deleted=min(numbox,truckSize)
+                truckSize-= deleted
+                numbox-= deleted
+                added-=uni*(deleted)
+            if numbox>0:
+                heapq.heappush(boxes,(uni,numbox))
+        return added
+            
+
+
+
+
+
+        
